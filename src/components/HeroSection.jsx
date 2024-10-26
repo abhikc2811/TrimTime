7360de5f75b383416264f93833659d84b6ccd033
Abhishek <3005010067.abhishek@gmail.com>
2024-10-26 11:01:54 +0530

    added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
         <h1 className=" display-4">Find the Best Barber Near You</h1>
         <p className="lead">Book appointments easily with trusted barbers in your area.</p>
         <div className="search-bar mt-4">
-          <input type="text" className="form-control" placeholder="Search for barbers, services, or locations" aria-label="Search" />
+          <input type="text" className="form-control search" placeholder="Search for barbers, services, or locations" aria-label="Search" />
           <button className="btn btn-primary mt-3">Search</button>
         </div>
       </section>
