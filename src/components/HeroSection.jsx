3a3c6d83b8c1be594cfd77d21b4c1ab48dfc98a1
Abhishek <3005010067.abhishek@gmail.com>
2024-11-19 18:06:41 +0530

    x

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,15 +1,14 @@
 const HeroSection = () => {
-    return (
-      <section className="hero bg-dark text-center p-5">
-        <h1 className=" display-4">Find the Best Barber Near You</h1>
-        <p className="lead">Book appointments easily with trusted barbers in your area.</p>
-        <div className="search-bar mt-4">
-          <input type="text" className="form-control search" placeholder="Search for barbers, services, or locations" aria-label="Search" />
-          <button className="btn btn-primary mt-3">Search</button>
-        </div>
-      </section>
-    );
-  };
-  
-  export default HeroSection;
-  
+  return (
+    <section className="hero bg-dark text-center p-5">
+      <h1 className="display-4">Find the Best Barber Near You</h1>
+      <p className="lead">Book appointments easily with trusted barbers in your area.</p>
+      <div className="search-bar mt-4 mx-auto d-flex" style={{ width: '50%' }}>
+        <input type="text" className="form-control search" placeholder="Search for barbers, services, or locations" aria-label="Search" />
+        <button className="btn btn-primary ms-2">Search</button>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
